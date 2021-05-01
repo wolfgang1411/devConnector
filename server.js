8e4c4d19cd131fb0c2868e847d4604f7b5b6fcec
wolfgang1411 <shoaibsaifi982@gmail,com>
2021-05-01 16:49:34 +0530

    fix server port fix

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.PORT || 5000
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
 
